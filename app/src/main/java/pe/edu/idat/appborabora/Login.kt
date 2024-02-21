a1353745ac0ea2e5c6d0d76f2efb8b7f8a26ca51
Brigitte <brigitteprietobravo20gmail.com>
2024-02-21 13:32:16 -0500

    diseño de login

---
+++ app/src/main/java/pe/edu/idat/appborabora/Login.kt
@@ -0,0 +1,11 @@
+package pe.edu.idat.appborabora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/pe/edu/idat/appborabora/NewPassword.kt
@@ -0,0 +1,11 @@
+package pe.edu.idat.appborabora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_password)
+    }
+}
