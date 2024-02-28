b7eae4294a46d099bf2e3a4957b8d219558a59f0
JDreyzen <jeffersonferre24@gmail.com>
2024-02-28 00:56:34 -0500

    prod-Comp

---
+++ app/src/main/java/pe/edu/idat/appborabora/view/fragments/DetalleProducto.kt
@@ -0,0 +1,21 @@
+package pe.edu.idat.appborabora.view.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import pe.edu.idat.appborabora.R
+
+
+class DetalleProducto : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detalle_producto, container, false)
+    }
+
+}
