f615caef627b551e4d405513dd9619b84d3063ca
Brigitte <brigitteprietobravo20gmail.com>
2024-03-01 19:09:24 -0500

    botones

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,42 +39,25 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.google.code.gson:gson:2.8.6")
-    //Glide
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    // Firebase Authentication
-    implementation ("com.google.firebase:firebase-auth:21.0.1")
-    //Carrusel
-    //implementation("com.github.smarteist:autoimageslider:1.4.0")
-    // Google Play services - Authentication
-    implementation ("com.google.android.gms:play-services-auth:19.2.0")
-    //Implementaciones de view model
-    implementation("androidx.lifecycle:lifecycle-viewmodel:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    //Implementación para convertir objetos JSON
-    implementation("com.google.code.gson:gson:2.8.9")
-    //Glide
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    //ROOM
-    implementation("androidx.room:room-runtime:2.6.1")
-    annotationProcessor("androidx.room:room-compiler:2.6.1")
-    testImplementation("androidx.room:room-testing:2.6.1")
-    //Sweet Alert
-    implementation ("com.github.f0ris.sweetalert:library:1.6.2")
-    //Slider
-    //implementation ("com.github.smarteist:autoimageslider:1.4.0")
+        implementation("androidx.core:core-ktx:1.9.0")
+        implementation("androidx.appcompat:appcompat:1.6.1")
+        implementation("com.google.android.material:material:1.11.0")
+        implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+        implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+        implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+        implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+        implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+        testImplementation("junit:junit:4.13.2")
+        androidTestImplementation("androidx.test.ext:junit:1.1.5")
+        androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+        implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+        implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+        implementation ("com.google.code.gson:gson:2.8.9")
+        implementation("com.github.bumptech.glide:glide:4.16.0")
+        implementation ("com.google.firebase:firebase-auth:21.0.1")
+        implementation ("com.google.android.gms:play-services-auth:19.2.0")
+        implementation("androidx.room:room-runtime:2.6.1")
+        annotationProcessor("androidx.room:room-compiler:2.6.1")
+        testImplementation("androidx.room:room-testing:2.6.1")
+        implementation ("com.github.f0ris.sweetalert:library:1.6.2")
 }
--- app/src/main/java/pe/edu/idat/appborabora/view/fragments/Dashboard.kt
+++ app/src/main/java/pe/edu/idat/appborabora/view/fragments/Dashboard.kt
@@ -1,20 +1,46 @@
 package pe.edu.idat.appborabora.view.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import pe.edu.idat.appborabora.R
+import pe.edu.idat.appborabora.view.activities.MainActivity
+import pe.edu.idat.appborabora.view.activities.Registro
 
-class Dashboard : Fragment() {
+class Dashboard : Fragment() , View.OnClickListener{
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnSesion = view.findViewById<Button>(R.id.btnsesion)
+        val btncuenta = view.findViewById<Button>(R.id.btncuenta)
+        btnSesion.setOnClickListener(this)
+        btncuenta.setOnClickListener(this)
+    }
+    override fun onClick(v: View?)
+
+    {
+        when (v?.id) {
+            R.id.btnsesion -> {
+                val intent = Intent(activity, MainActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.btncuenta -> {
+                val intent = Intent(activity, Registro::class.java)
+                startActivity(intent)
+            }
+        }
     }
 
 
