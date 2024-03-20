1655cb4560bf271b34069dee2197aac77515ae46
JDreyzen <jeffersonferre24@gmail.com>
2024-03-20 01:31:28 -0500

    correcion de categorias

--- app/src/main/java/pe/edu/idat/appborabora/data/model/response/CategoryResponse.kt
+++ app/src/main/java/pe/edu/idat/appborabora/data/model/response/CategoryResponse.kt
@@ -3,7 +3,5 @@ package pe.edu.idat.appborabora.data.model.response
 data class CategoryResponse(
     val id_category: Int,
     val name: String,
-    val image: String
-
-
+    val image: String,
 )
