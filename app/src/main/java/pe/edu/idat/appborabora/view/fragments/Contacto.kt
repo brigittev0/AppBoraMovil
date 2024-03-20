aeadb5cf8945911e772f1748af44acd79fbce2b1
jhon123f <floresrjhon74@gmail.com>
2024-03-20 16:00:11 -0500

    contacto

--- app/src/main/java/pe/edu/idat/appborabora/view/fragments/Contacto.kt
+++ app/src/main/java/pe/edu/idat/appborabora/view/fragments/Contacto.kt
@@ -1,20 +1,49 @@
 package pe.edu.idat.appborabora.view.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import pe.edu.idat.appborabora.R
+import pe.edu.idat.appborabora.databinding.FragmentContactoBinding
 
-class Contacto : Fragment() {
+class Contacto : Fragment(), View.OnClickListener {
+
+    private var _binding: FragmentContactoBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacto, container, false)
+    ): View {
+        _binding = FragmentContactoBinding.inflate(inflater, container, false)
+        binding.btnChatear.setOnClickListener(this)
+        binding.btnLlamar.setOnClickListener(this)
+        return binding.root
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btnChatear -> abrirChatWhatsApp()
+            R.id.btnLlamar -> realizarLlamada()
+        }
+    }
+
+    private fun abrirChatWhatsApp() {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse("https://wa.me/51904392864")
+        }
+        startActivity(intent)
+    }
+
+    private fun realizarLlamada() {
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:51904392864")
+        }
+        startActivity(intent)
     }
 
 }
--- app/src/main/java/pe/edu/idat/appborabora/view/fragments/Perfil.kt
+++ app/src/main/java/pe/edu/idat/appborabora/view/fragments/Perfil.kt
@@ -1,13 +1,22 @@
 package pe.edu.idat.appborabora.view.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import pe.edu.idat.appborabora.R
 
 class Perfil : Fragment() {
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var nombreEditText: EditText
+    private lateinit var apellidoEditText: EditText
+    private lateinit var dniEditText: EditText
+    private lateinit var emailEditText: EditText
+    private lateinit var celularEditText: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
