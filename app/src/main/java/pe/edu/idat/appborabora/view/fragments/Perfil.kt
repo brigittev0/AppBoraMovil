d7777840ead318d49a12ce1e6999f993d0c19dd8
jhon123f <floresrjhon74@gmail.com>
2024-03-23 21:21:37 -0500

    updatedetalles

--- app/src/main/java/pe/edu/idat/appborabora/view/fragments/Perfil.kt
+++ app/src/main/java/pe/edu/idat/appborabora/view/fragments/Perfil.kt
@@ -1,11 +1,9 @@
 package pe.edu.idat.appborabora.view.fragments
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import androidx.fragment.app.Fragment
 import pe.edu.idat.appborabora.R
 
