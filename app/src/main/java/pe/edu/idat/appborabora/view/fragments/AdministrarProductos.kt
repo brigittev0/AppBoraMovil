0cf4938e7376787e8016f80a3dc06ecc0716b310
brigittev0 <brigitteprietobravo20@gmail.com>
2024-04-04 17:32:19 -0500

    añadiendo funcionamiento para que los botones de administrar productos se dirigan a sus respectivos fragments

--- app/src/main/java/pe/edu/idat/appborabora/view/fragments/AdministrarProductos.kt
+++ app/src/main/java/pe/edu/idat/appborabora/view/fragments/AdministrarProductos.kt
@@ -5,15 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 import pe.edu.idat.appborabora.R
+import pe.edu.idat.appborabora.databinding.FragmentAdministrarProductosBinding
 
 class AdministrarProductos : Fragment() {
+    private var _binding: FragmentAdministrarProductosBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_administrar_productos, container, false)
+    ): View {
+        _binding = FragmentAdministrarProductosBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Navega mediante el boton hacia el fragment
+        binding.btnCrear.setOnClickListener {
+            findNavController().navigate(R.id.crearProducto)
+        }
+        binding.btnActualizar.setOnClickListener {
+            findNavController().navigate(R.id.actualizarProducto)
+        }
+        binding.btnEliminar.setOnClickListener {
+            findNavController().navigate(R.id.eliminarProducto)
+        }
+        binding.btnVer.setOnClickListener {
+            findNavController().navigate(R.id.verProductos)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
---
+++ app/src/main/java/pe/edu/idat/appborabora/view/fragments/CrearProducto.kt
@@ -0,0 +1,18 @@
+package pe.edu.idat.appborabora.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pe.edu.idat.appborabora.R
+
+class CrearProducto : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_crear_producto, container, false)
+    }
+
+}
