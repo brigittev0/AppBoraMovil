53cc3801016e47d8f0ba4189f470169c7b3ab880
CarlosAcosta4 <carlosacosta4820@gmail.com>
2024-04-07 16:18:33 -0500

    Eliminacion de las constantes de las preferencias compartidas

--- app/src/main/java/pe/edu/idat/appborabora/util/Constants.kt
+++ app/src/main/java/pe/edu/idat/appborabora/util/Constants.kt
@@ -5,15 +5,5 @@ object Constants {
     //Base URL
     const val BASE_URL = "http://192.168.18.8:8070/api/v1/"
 
-    //Valores del Usuario Logueado
-    const val PREF_NAME = "UsuarioLogueado"
-    const val KEY_JWT = "jwt"
-    const val KEY_ROLE = "role"
-    const val KEY_IDENTITY_DOC = "identityDoc"
-
-    //Valores por defecto
-    const val DEFAULT_JWT = ""
-    const val DEFAULT_ROLE = "Invitado"
-    const val DEFAULT_IDENTITY_DOC = -1
 }
 
