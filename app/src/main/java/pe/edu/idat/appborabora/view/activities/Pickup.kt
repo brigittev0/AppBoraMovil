28fef1ea54bbff24cca295f0f52a9ccb51ed3980
CarlosAcosta4 <carlosacosta4820@gmail.com>
2024-04-10 23:52:05 -0500

    + Flecha de retroceso en el toolbar de pickup y delivery

--- app/src/main/java/pe/edu/idat/appborabora/view/activities/Pickup.kt
+++ app/src/main/java/pe/edu/idat/appborabora/view/activities/Pickup.kt
@@ -2,11 +2,33 @@ package pe.edu.idat.appborabora.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import pe.edu.idat.appborabora.R
 
 class Pickup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pickup)
+
+        //--Toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.title = "Retiro en tienda"
+        //--
+    }
+
+    // Maneja el clic en la flecha de retroceso
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
