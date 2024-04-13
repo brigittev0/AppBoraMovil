bb89401a0975fadcaf78003b6181f5ad0954a6d4
jhon123f <floresrjhon74@gmail.com>
2024-04-13 10:51:35 -0500

    carrusel

--- app/src/main/java/pe/edu/idat/appborabora/slider/SliderItem.kt
+++ app/src/main/java/pe/edu/idat/appborabora/slider/SliderItem.kt
@@ -1,13 +1,3 @@
 package pe.edu.idat.appborabora.slider
 
-class SliderItem {
-    var titulo: String? = null
-    var imagen: Int = 0
-
-    constructor()
-
-    constructor(imagen: Int, titulo: String) {
-        this.imagen = imagen
-        this.titulo = titulo
-    }
-}
+data class SliderItem(val image: Int)
