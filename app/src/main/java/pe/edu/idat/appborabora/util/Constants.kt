e3280ea28b8ac477479a7e01837bae1990ebefa4
CarlosAcosta4 <carlosacosta4820@gmail.com>
2024-04-14 21:32:15 -0500

    Conflictos resueltos

--- app/src/main/java/pe/edu/idat/appborabora/util/Constants.kt
+++ app/src/main/java/pe/edu/idat/appborabora/util/Constants.kt
@@ -4,11 +4,7 @@ object Constants {
 
     //-- AGREGAR TU IP
     //Base URL
-<<<<<<< HEAD
     const val BASE_URL = "http://192.168.18.8:8070/api/v1/"
-=======
-    const val BASE_URL = "http://192.168.1.36:8070/api/v1/"
->>>>>>> c879a550824952d7cbcf10ad9917ee26ceb4465c
 
 }
 
